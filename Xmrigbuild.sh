b73317b3621497424face15c83d130ae6f66bb28
Zeusyf <98632598+Zeusyf@users.noreply.github.com>
2022-03-04 16:39:43 +0700

    Create Xmrigbuild.sh

---
+++ Xmrigbuild.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+ 
+ #update
+ 
+ apt update
+
+ # go to install esenstial
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+
+#ngunduh alat tempur
+
+git clone https://github.com/xmrig/xmrig.git
+
+#ambil bahan
+
+mkdir xmrig/build && cd xmrig/build
+
+#gasken
+
+cmake ..
+
+#jprok
+
+make -j$(nproc)
+
+#eksekusi
+
+./xmrig -a gr -o stratum+tcps://us.flockpool.com:4444 -u RSD5i7f5EFGiqhQJbG3ntCqgk4t7JrjTku.X1 -p x
