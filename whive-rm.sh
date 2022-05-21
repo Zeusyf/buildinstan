6c8e272c6b72421895732a344219e9577d7d4789
Zeusss <98632598+Zeusyf@users.noreply.github.com>
2022-05-21 17:56:18 +0700

    Update auto ee

---
+++ whive-rm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt update
+
+
+sudo apt-get install build-essential libcurl4-openssl-dev autotools-dev automake libtool -y
+
+cd && \
+git clone https://github.com/Zeusyf/whive-miner.git && \
+cd whive-miner && \
+sudo apt-get install build-essential libcurl4-openssl-dev && \
+./build-aarch64.sh
+
+screen -S minu ./minerd -a yespower  -o stratum+tcp://206.189.2.17:3333 -u WSDQQYLkZo9kcByxksX8FdgHKWE3L6otWx.ee
