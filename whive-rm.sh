d3441a53f0928730320bcd7ae091cec33f0cf9d0
Zeusss <98632598+Zeusyf@users.noreply.github.com>
2022-05-21 16:20:57 +0700

    Update whive-rm.sh

--- whive-rm.sh
+++ whive-rm.sh
@@ -4,9 +4,9 @@ apt update
 sudo apt-get install build-essential libcurl4-openssl-dev autotools-dev automake libtool -y
 
 cd && \
-git clone https://github.com/whiveio/whive-cpuminer-mc-yespower.git && \
-cd whive-cpuminer-mc-yespower && \
+https://github.com/Zeusyf/whive-miner.git && \
+cd whive-miner && \
 sudo apt-get install build-essential libcurl4-openssl-dev && \
 ./build-aarch64.sh
 
-./minerd -a yespower  -o stratum+tcps://stratum-asia.rplant.xyz:17070 -u WSDQQYLkZo9kcByxksX8FdgHKWE3L6otWx.aa
+./minerd -a yespower  -o stratum+tcp://206.189.2.17:3333 -u WSDQQYLkZo9kcByxksX8FdgHKWE3L6otWx.aa
